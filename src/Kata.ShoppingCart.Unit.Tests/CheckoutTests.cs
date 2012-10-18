c49845be4dc63b76203314696b8ce8bb01438979
Chris Watson <chris.watson@7digital.com>
2012-10-18 16:27:34 +0100

    initial commit

--- src/Kata.ShoppingCart.Unit.Tests/CheckoutTests.cs
+++ src/Kata.ShoppingCart.Unit.Tests/CheckoutTests.cs
@@ -6,11 +6,5 @@ namespace Kata.ShoppingCart.Unit.Tests
     [TestFixture]
     public class CheckoutTests
     {
-        [Test]
-        public void EmptyCheckoutHasZeroTotal()
-        {
-            var mock = new Mock<IDiscounter>();
-            Assert.That(0, Is.EqualTo(new Checkout(mock.Object).Total()));
-        }
     }
 }
